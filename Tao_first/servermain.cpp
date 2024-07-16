29ef8aceeef79e38a3b9cc12f50b8393a9f5d2ac
blowfishfugu <coolm12345@hotmail.de>
2024-07-17 00:19:23 +0200

    lokalversuche mit tao_naming.exe, portvorgabe 2809, bind(Name), resolve(Name), viele grüne Todos ergänzt

--- Tao_first/servermain.cpp
+++ Tao_first/servermain.cpp
@@ -19,6 +19,8 @@
 #include <algorithm>
 #include <iostream>
 #include <chrono>
+#include <array>
+
 using namespace std::chrono_literals;
 
 static auto StoreArgs = [](int argc, char** argv) {
@@ -31,17 +33,89 @@ static auto StoreArgs = [](int argc, char** argv) {
 		});
 	return args;
 };
+class MyPerson : public virtual POA_CompanyModule::Person
+{
+public:
+	std::string _firstname{};
+	std::string _name{};
+	std::chrono::year_month_day _birthdate{ 1990y/3/12d };
+	
+	virtual ~MyPerson() {
+		std::cout << "deleted MyPerson\n";
+	}
+
+	// Inherited via Person
+	virtual char* firstname() override
+	{
+		return _firstname.data();
+	}
+	virtual void firstname(const char* firstname) override
+	{
+		_firstname =std::string(firstname);
+	}
+	virtual char* name() override
+	{
+		return _name.data();
+	}
+	virtual void name(const char* name) override
+	{
+		_name = std::string(name);
+	}
+	virtual ::CompanyModule::YearMonthDay birthday() override
+	{
+		return ::CompanyModule::YearMonthDay{
+			.year = static_cast<CORBA::Long>( _birthdate.year() ),
+			.month = static_cast<CORBA::UShort>( (unsigned int)_birthdate.month() ),
+			.day = static_cast<CORBA::UShort>( (unsigned int)_birthdate.day() )
+		};
+	}
+	virtual void birthday(const::CompanyModule::YearMonthDay& birthday) override
+	{
+		_birthdate = { std::chrono::year{birthday.year} / birthday.month / birthday.day };
+	}
+	virtual char* FullName() override
+	{
+		thread_local std::string fullname( std::format("{} {}", _firstname.c_str(), _name.c_str()));
+		return fullname.data();
+	}
+};
+
+class MyEmployee : public virtual POA_CompanyModule::Employee, public virtual MyPerson
+{
+public:
+	virtual ~MyEmployee() {
+		std::cout << "deleted MyEmpoyee\n";
+	}
+
+	double _salary{};
+	// Inherited via Employee
+	virtual ::CORBA::Double salary() override
+	{
+		return _salary;
+	}
+	virtual void salary(::CORBA::Double salary) override
+	{
+		_salary = salary;
+	}
+
+	virtual void payout() override
+	{
+		_salary = {};
+	}
+};
 
 class MyFactory : public virtual POA_CompanyModule::ImpFactory
 {
 	// Inherited via ImpFactory
 	virtual ::CompanyModule::Person_ptr CreatePerson() override
 	{
-		return ::CompanyModule::Person_ptr();
+		MyPerson* person = new MyPerson(); //TODO: <- das kann nicht richtig sein
+		return CompanyModule::Person::_duplicate(person->_this());
 	}
 	virtual ::CompanyModule::Employee_ptr CreateEmployee() override
 	{
-		return ::CompanyModule::Employee_ptr();
+		MyEmployee* emp = new MyEmployee(); //TODO: <- das kann nicht richtig sein
+		return CompanyModule::Employee::_duplicate(emp->_this());
 	}
 };
 
@@ -52,7 +126,13 @@ int main(int argc, char** argv)
 	//poa
 	//poa-activate
 	//orbdestroy
-	CORBA::ORB_var orb = CORBA::ORB_init(argc, argv); //<-achtung, argv hiernach weg!
+
+	std::array<const char*, 2> pArgs{
+		argv[0],
+		"-ORBInitRef NameService=corbaloc:iiop:localhost:2809/NameService",
+	};
+	int argSize = static_cast<int>( pArgs.size() );
+	CORBA::ORB_var orb = CORBA::ORB_init(argSize, const_cast<char**>(pArgs.data())); //<-achtung, argv hiernach weg?
 	
 	CORBA::Object_var poa_object = orb->resolve_initial_references("RootPOA"); //<- hier der hit auf firewall-abfrage
 	PortableServer::POA_var poa = PortableServer::POA::_narrow(poa_object /*.in() */);
@@ -73,15 +153,16 @@ int main(int argc, char** argv)
 	std::string iorStr( orb->object_to_string(pObj) );
 	std::cout << iorStr << "\n";
 
-	//TODO: try-catch und in eigene funktion auslagern
+	//TODO: try-catch und in eigene funktion auslagern, TryRegister(const char* Name, pObj)
 	try 
 	{
 		//doku: file:///$TAO_ROOT/docs/tutorials/Quoter/Naming_Service/index.html
+		// spec: https://www.omg.org/cgi-bin/doc?ptc/00-08-07   -> Nameservice defaultport: 2809
 		//"wir" sollten starten mit:
-		// servermain.exe -ORBInitRef NameService=corbaloc:iiop:localhost:12345/NameService
+		// servermain.exe -ORBInitRef NameService=corbaloc:iiop:localhost:2809/NameService
 		//und der namingservice z.B. mit:
-		// $TAO_ROOT/orbsvcs/Naming_Service/tao_cosnaming -ORBEndPoint iiop://localhost:12345
-		ACE_Time_Value timeout(40ms); //lokales LAN
+		// $TAO_ROOT/orbsvcs/Naming_Service/tao_cosnaming -m 1 -ORBEndPoint iiop://localhost:2809
+		ACE_Time_Value timeout(40ms); //lokales LAN, ping auf 1ms, 40ms warten dürfte reichen
 		CORBA::Object_var ns_obj = orb->resolve_initial_references("NameService", &timeout);
 		if (CORBA::is_nil(ns_obj))
 		{
@@ -90,11 +171,38 @@ int main(int argc, char** argv)
 			return 1;
 		}
 		CosNaming::NamingContext_var ns_context = CosNaming::NamingContext::_narrow(ns_obj);
+		//ns_context->list(1024, ) //TODO: list_names(ns_context.in()) ala tao/utils/nslist/nslist.cpp
+		
+		
+		//TODO: toName(const char*)->CosNaming::Name
+		CosNaming::Name Name(1);
+		Name.length(1);
+		Name[0].id = CORBA::string_dup("MyFactory");
+		ns_context->bind(Name, pObj);
+		
+		//TODO: separate Funktion try_resolve(ns_context, const char* name)
+		CORBA::Object_var tstResolve = ns_context->resolve(Name);
+		CompanyModule::ImpFactory_var iFactory=CompanyModule::ImpFactory::_narrow(tstResolve);
+		tstResolve = nullptr;
+
+		CompanyModule::Person_var person=iFactory->CreatePerson();
+		
+		ns_context->unbind(Name); //<- kann das jeder client?
 	}
-	catch (CORBA::ORB::InvalidName exc) //TODO: welchen Typ wirft der timeout?
+	catch (CORBA::ORB::InvalidName exc) //TODO: welchen Typ wirft der timeout? InvalidName ist abgehandelt :)
 	{
 		std::cout << "failed to resolve NameService\n" << std::string(exc._info().c_str()) << "\n";
 	}
+
+	std::jthread runFor([&orb]() {
+		for (int i = 0; i < 5; ++i)
+		{
+			std::cout << std::format("shutdown in {} ..\n", 5 - i);
+			std::this_thread::sleep_for(1s);
+		}
+		orb->shutdown(true);
+		});
+
 	//dies hier, wie der windows-messageloop, blocking listener auf events
 	orb->run(); //<- ab hier
 	
